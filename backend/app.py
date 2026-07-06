348001c1bae831301f130e9c14312f720b44e1ba
preethiande546 <preethiande546@gmail.com>
2026-07-07 01:13:52 +0530

    Fixed CORS configuration

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ init_db()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
